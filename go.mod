784023795bfcc17a1467c6f90292785865c0ef7b
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-05-22 16:28:55 -0400

    Use ocm clusters mgmt and osd fleet mgmt api to get upgrade versions

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ryankwilliams/validate-mcsc-upgrades
 go 1.19
 
 require (
+	github.com/Masterminds/semver v1.5.0
 	github.com/onsi/ginkgo/v2 v2.9.4
 	github.com/onsi/gomega v1.27.6
 	github.com/openshift/osde2e-framework v0.0.0
@@ -11,7 +12,6 @@ require (
 require google.golang.org/protobuf v1.30.0 // indirect
 
 require (
-	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
@@ -19,12 +19,21 @@ require (
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
@@ -33,25 +42,48 @@ require (
 	github.com/hashicorp/hc-install v0.5.2 // indirect
 	github.com/hashicorp/terraform-exec v0.18.1 // indirect
 	github.com/hashicorp/terraform-json v0.15.0 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.18 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/openshift-online/ocm-sdk-go v0.1.340 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.15.1 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.43.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/zclconf/go-cty v1.13.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.26.3 // indirect
+	k8s.io/apimachinery v0.26.3 // indirect
+	k8s.io/client-go v0.26.3 // indirect
+	k8s.io/klog/v2 v2.90.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
+	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
+	sigs.k8s.io/controller-runtime v0.14.6 // indirect
+	sigs.k8s.io/e2e-framework v0.2.0 // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 replace github.com/openshift/osde2e-framework v0.0.0 => /home/rywillia/sdqe/osde2e-framework
--- tests/mcscupgrades.go
+++ tests/mcscupgrades.go
@@ -5,71 +5,138 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Masterminds/semver"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 
 	"github.com/openshift/osde2e-framework/pkg/clients/ocm"
+	"github.com/openshift/osde2e-framework/pkg/providers/osd"
+	"github.com/openshift/osde2e-framework/pkg/providers/rosa"
 
 	"github.com/ryankwilliams/validate-mcsc-upgrades/internal/labels"
-	"github.com/ryankwilliams/validate-mcsc-upgrades/internal/provider"
 )
 
 var (
-	scUpgradeVersion *string
-	mcUpgradeVersion *string
-	p                *provider.Provider
+	managementClusterID             string
+	managementClusterVersion        semver.Version
+	managementClusterUpgradeVersion semver.Version
+	serviceClusterID                string
+	serviceClusterVersion           semver.Version
+	serviceClusterUpgradeVersion    semver.Version
+	scUpgradeVersion                *string
+	mcUpgradeVersion                *string
+	osdProvider                     *osd.Provider
+	rosaProvider                    *rosa.Provider
+	hcpClusterID                    *string
+)
+
+const (
+	clusterName    = "my-cluster"
+	clusterVersion = "4.12.18"
+	channelGroup   = "candidate"
 )
 
 var _ = ginkgo.BeforeSuite(func() {
 	var (
-		clusterVersion string
-		err            error
+		err error
 
-		ctx      = context.Background()
-		ocmEnv   = ocm.Stage
-		ocmToken = os.Getenv("OCM_TOKEN")
+		ctx                             = context.Background()
+		osdFleetMgmtManagementClusterID = os.Getenv("OSD_FLEET_MGMT_MANAGEMENT_CLUSTER_ID")
+		ocmEnv                          = ocm.Integration
+		ocmToken                        = os.Getenv("OCM_TOKEN")
+		provisionShardID                = os.Getenv("PROVISION_SHARD_ID")
+		osdFleetMgmtServiceClusterID    = os.Getenv("OSD_FLEET_MGMT_SERVICE_CLUSTER_ID")
+		upgradeType                     = os.Getenv("UPGRADE_TYPE")
 	)
 
 	// Construct new rosa provider
-	p, err = provider.New(
-		ctx,
-		ocmToken,
-		ocmEnv,
-		&provider.Cluster{
-			Name:         "my-cluster",
-			Version:      "4.12.6",
-			ChannelGroup: "candidate",
-		},
-	)
+	rosaProvider, err = rosa.New(ctx, ocmToken, ocmEnv)
 	gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to construct rosa provider")
 
-	// Connect to MC/SC
+	// Construct new osd provider
+	osdProvider, err = osd.New(ctx, ocmToken, ocmEnv)
+	gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to construct osd provider")
 
-	// Determine service cluster install version
+	// Verify required data was provided
+	gomega.Expect(osdFleetMgmtServiceClusterID).Error().ShouldNot(gomega.BeEmpty(), "service cluster id was not provided")
+	gomega.Expect(osdFleetMgmtManagementClusterID).Error().ShouldNot(gomega.BeEmpty(), "management cluster id was not provided")
+	gomega.Expect(provisionShardID).Error().ShouldNot(gomega.BeEmpty(), "provision shard id was not provided") // TODO: Open issue/pr to expose this field when using ocm-sdk-go
+	gomega.Expect(upgradeType).Error().ShouldNot(gomega.BeEmpty(), "upgrade type was not provided, must be either 'Y' or 'Z'")
+	gomega.Expect(upgradeType).Error().Should(gomega.BeElementOf([]string{"Y", "Z"}), "upgrade type is invalid")
+
+	// TODO: Consolidate these
+
+	// Identify the service cluster install/upgrade versions
 	if labels.SCUpgrade.MatchesLabelFilter(ginkgo.GinkgoLabelFilter()) {
-		if clusterVersion, err = provider.IdentifyClusterVersion(); err != nil {
-			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to identify service cluster version")
+		osdFleetManagerSC, err := osdProvider.OSDFleetMgmt().V1().ServiceClusters().ServiceCluster(osdFleetMgmtServiceClusterID).Get().SendContext(ctx)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to get osd fleet manager service cluster")
+
+		serviceCluster, err := osdProvider.ClustersMgmt().V1().Clusters().Cluster(osdFleetManagerSC.Body().ClusterManagementReference().ClusterId()).Get().SendContext(ctx)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to get service cluster")
+
+		serviceClusterID = serviceCluster.Body().Version().RawID()
+		serviceClusterVersion, err := semver.NewVersion(serviceClusterID)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to parse version to semantic version")
+
+		availableVersions := serviceCluster.Body().Version().AvailableUpgrades()
+		totalAvailableVersions := len(availableVersions)
+		gomega.Expect(totalAvailableVersions).ToNot(gomega.BeNumerically("==", 0), "service cluster has no available supported upgrade versions")
+
+		for i := 0; i < totalAvailableVersions; i++ {
+			version, err := semver.NewVersion(availableVersions[totalAvailableVersions-i-1])
+			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to parse version to semantic version")
+			if (serviceClusterVersion.Minor() == version.Minor()) && upgradeType == "Z" {
+				serviceClusterUpgradeVersion = *version
+				break
+			} else if (serviceClusterVersion.Minor() < version.Minor()) && upgradeType == "Y" {
+				serviceClusterUpgradeVersion = *version
+				break
+			}
 		}
 
-		if scUpgradeVersion, err = provider.DetermineUpgradeVersion(clusterVersion); err != nil {
-			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to identify service cluster upgrade version")
-		}
+		gomega.Expect(serviceClusterUpgradeVersion).ToNot(gomega.BeNil(), "unable to identify service cluster upgrade version")
 	}
 
-	// Determine management cluster upgrade version
+	// Identify the management cluster install/upgrade versions
 	if labels.MCUpgrade.MatchesLabelFilter(ginkgo.GinkgoLabelFilter()) {
-		if clusterVersion, err = provider.IdentifyClusterVersion(); err != nil {
-			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to identify management cluster version")
+		osdFleetManagerMC, err := osdProvider.OSDFleetMgmt().V1().ManagementClusters().ManagementCluster(osdFleetMgmtManagementClusterID).Get().SendContext(ctx)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to get osd fleet manager management cluster")
+
+		managementCluster, err := osdProvider.ClustersMgmt().V1().Clusters().Cluster(osdFleetManagerMC.Body().ClusterManagementReference().ClusterId()).Get().SendContext(ctx)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to get management cluster")
+
+		managementClusterID = managementCluster.Body().Version().RawID()
+		managementClusterVersion, err := semver.NewVersion(managementClusterID)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to parse version to semantic version")
+
+		availableVersions := managementCluster.Body().Version().AvailableUpgrades()
+		totalAvailableVersions := len(availableVersions)
+		gomega.Expect(totalAvailableVersions).ToNot(gomega.BeNumerically("==", 0), "management cluster has no available supported upgrade versions")
+
+		for i := 0; i < totalAvailableVersions; i++ {
+			version, err := semver.NewVersion(availableVersions[totalAvailableVersions-i-1])
+			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to parse version to semantic version")
+			if (managementClusterVersion.Minor() == version.Minor()) && upgradeType == "Z" {
+				managementClusterUpgradeVersion = *version
+				break
+			} else if (managementClusterVersion.Minor() < version.Minor()) && upgradeType == "Y" {
+				managementClusterUpgradeVersion = *version
+				break
+			}
 		}
 
-		if mcUpgradeVersion, err = provider.DetermineUpgradeVersion(clusterVersion); err != nil {
-			gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "unable to identify management cluster upgrade version")
-		}
+		gomega.Expect(managementClusterUpgradeVersion).ToNot(gomega.BeNil(), "unable to identify management cluster upgrade version")
 	}
 
 	if labels.ApplyHCPWorkloads.MatchesLabelFilter(ginkgo.GinkgoLabelFilter()) {
-		err := p.CreateHCPClusters(ctx)
+		hcpClusterID, err := rosaProvider.CreateCluster(ctx, &rosa.CreateClusterOptions{
+			ClusterName:  clusterName,
+			Version:      clusterVersion,
+			ChannelGroup: channelGroup,
+			HostedCP:     true,
+		})
 		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "create hcp cluster failed")
+		fmt.Printf("HCP cluster %q created\n", hcpClusterID)
 	}
 })
 
@@ -77,43 +144,49 @@ var _ = ginkgo.AfterSuite(func() {
 	ctx := context.Background()
 
 	defer func() {
-		_ = p.Connection.Close()
+		_ = osdProvider.Connection.Close()
+		_ = rosaProvider.Connection.Close()
 	}()
 
 	if labels.RemoveHCPWorkloads.MatchesLabelFilter(ginkgo.GinkgoLabelFilter()) {
-		err := p.DeleteHCPClusters(ctx)
+		err := rosaProvider.DeleteCluster(ctx, &rosa.DeleteClusterOptions{
+			ClusterName: clusterName,
+			ClusterID:   *hcpClusterID,
+			HostedCP:    true,
+		})
 		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "delete hcp cluster failed")
 	}
 })
 
 var _ = ginkgo.Describe("HyperShift", ginkgo.Ordered, func() {
-
-	ginkgo.It("service cluster is upgraded successfully", labels.SCUpgrade, func() {
+	ginkgo.It("service cluster is upgraded successfully", labels.SCUpgrade, func(ctx context.Context) {
 		fmt.Printf("Performing service cluster upgrade to version %q\n", *scUpgradeVersion)
 		gomega.Expect(true).Should(gomega.BeTrue())
 	})
 
-	ginkgo.It("service cluster health checks are passing post upgrade", labels.SCUpgrade, labels.SCUpgradeHealthChecks, func() {
+	ginkgo.It("service cluster health checks are passing post upgrade", labels.SCUpgrade, labels.SCUpgradeHealthChecks, func(ctx context.Context) {
 		fmt.Println("Performing service cluster post upgrade health checks")
-		gomega.Expect(true).Should(gomega.BeTrue())
+		err := osdProvider.OCMUpgrade(ctx, serviceClusterID, serviceClusterVersion, serviceClusterUpgradeVersion)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "service cluster upgrade failed")
 	})
 
-	ginkgo.It("hcp workloads are unaffected post service cluster upgrade", labels.SCUpgrade, func() {
+	ginkgo.It("hcp workloads are unaffected post service cluster upgrade", labels.SCUpgrade, func(ctx context.Context) {
 		fmt.Println("Performing hcp cluster post service cluster upgrade")
 		gomega.Expect(true).Should(gomega.BeTrue())
 	})
 
-	ginkgo.It("management cluster is upgraded successfully", labels.MCUpgrade, func() {
+	ginkgo.It("management cluster is upgraded successfully", labels.MCUpgrade, func(ctx context.Context) {
 		fmt.Printf("Performing management cluster upgrade to version %q\n", *mcUpgradeVersion)
-		gomega.Expect(true).Should(gomega.BeTrue())
+		err := osdProvider.OCMUpgrade(ctx, managementClusterID, managementClusterVersion, managementClusterUpgradeVersion)
+		gomega.Expect(err).Error().ShouldNot(gomega.HaveOccurred(), "management cluster upgrade failed")
 	})
 
-	ginkgo.It("management cluster health checks are passing post upgrade", labels.MCUpgrade, labels.MCUpgradeHealthChecks, func() {
+	ginkgo.It("management cluster health checks are passing post upgrade", labels.MCUpgrade, labels.MCUpgradeHealthChecks, func(ctx context.Context) {
 		fmt.Println("Performing management cluster post upgrade health checks")
 		gomega.Expect(true).Should(gomega.BeTrue())
 	})
 
-	ginkgo.It("hcp workloads are unaffected post management cluster upgrade", labels.MCUpgrade, func() {
+	ginkgo.It("hcp workloads are unaffected post management cluster upgrade", labels.MCUpgrade, func(ctx context.Context) {
 		fmt.Println("Performing hcp cluster post management cluster upgrade")
 		gomega.Expect(true).Should(gomega.BeTrue())
 	})
