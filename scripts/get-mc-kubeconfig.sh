ccdefe4ed530b3ec354721507c28d3ba5c098697
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-05-24 15:40:29 -0400

    Add new scripts to get mc/sc kubeconfigs

---
+++ scripts/get-mc-kubeconfig.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $OCM_TOKEN ];
+then
+    echo "OCM_TOKEN is not set, please set it and try again"
+    exit 1
+fi
+
+CLUSTER_ID=$1
+if [ -z $CLUSTER_ID ];
+then
+    echo "CLUSTER_ID is not set, please set it and try again"
+    exit 1
+fi
+
+ocm login --token $OCM_TOKEN --url integration
+cluster_href=`ocm get /api/osd_fleet_mgmt/v1/management_clusters/$CLUSTER_ID | jq -r .cluster_management_reference.href`
+ocm get $cluster_href/credentials | jq -r .kubeconfig > $CLUSTER_ID-kubeconfig
